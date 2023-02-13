cd7f8d76e24b8dcec43fedfc6b4d0bc4635b1e69
siddhantmendhe <38434303+siddhantmendhe@users.noreply.github.com>
2023-02-13 11:00:14 +0530

    close dropdown based on clicked location

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import { useState } from "react";
 import Dropdown from "./components/Dropdown";
 function App(){
+    const [selection, setSelection]=useState(null);
+
+    const handleSelect=(option)=>{
+        setSelection(option);
+    }
     const options=[
-        {lable:'Red', value:'red'},
-        {lable: 'Green', value: 'green'},
-        {lable: 'Blue', value: 'blue'}
+        {label:'Red', value:'red'},
+        {label: 'Green', value: 'green'},
+        {label: 'Blue', value: 'blue'}
     ]
-    return <Dropdown options={options}/>;
+    return (
+        <div className="flex">
+    <Dropdown options={options} value={selection} onChange={handleSelect}/>
+    <Dropdown options={options} value={selection} onChange={handleSelect}/>
+    </div>);
 }
 
 export default App;
