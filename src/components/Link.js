94e4f766b03449db79c8068cc7ae24e23d931287
siddhantmendhe <38434303+siddhantmendhe@users.noreply.github.com>
2023-02-18 17:53:03 +0530

    link classes, custom hook

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,16 +1,21 @@
-import { useContext } from 'react';
-import NavigationContext from '../context/navigation';
+import classNames from 'classnames';
+import useNavigation from '../hooks/use-navigation';
 
 function Link({ to, children }) {
-  const { navigate } = useContext(NavigationContext);
+  const { navigate } = useNavigation();
+  const className=classNames('text-blue-500');
 
   const handleClick = (event) => {
+    if(event.ctrlKey||event.metaKey){
+
+      return;
+    }
     event.preventDefault();
 
     navigate(to);
   };
 
-  return <a href="demo" onClick={handleClick}>{children}</a>;
+  return <a className={className} href={to} onClick={handleClick}>{children}</a>;
 }
 
 export default Link;
