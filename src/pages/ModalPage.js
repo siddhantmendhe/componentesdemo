5216df305a6d4f2bda6d3afa3be7a20e6b6a237e
siddhantmendhe <38434303+siddhantmendhe@users.noreply.github.com>
2023-02-20 10:48:03 +0530

    modal Accept button

--- src/pages/ModalPage.js
+++ src/pages/ModalPage.js
@@ -8,11 +8,26 @@ function ModalPage(){
     const handleClick=()=>{
         setShowModal(true);
     };
+    const handleClose=()=>{
+        setShowModal(false);
+    }
+
+    const actionBar=(
+        <div>
+            <Button onClick={handleClose} primary>I Accept</Button>
+        </div>
+    )
+
+    const modal=(
+        <Modal onClose={handleClose} actionBar={actionBar}>
+        <p>Here is an import agreement for you to accept</p>
+        </Modal>
+    )
 
     return (<div>
         <Button onClick={handleClick} primary>
             Open Modal</Button>
-        {showModal && <Modal/>}
+        {showModal && modal}
         </div>)
 }
 
